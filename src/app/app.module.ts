15ce87db87a5680f5b4c69952a778231b217d7b0
3eJI9 <97410441+3eJI9@users.noreply.github.com>
2022-05-12 20:57:40 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {Route, RouterModule} from "@angular/router";
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {ComentDetailsComponent} from './components/coment-details/coment-details.component';
+import {HeaderComponent} from './components/header/header.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+
+
+const routes: Route[] = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+
+      {path: 'users', component: UserComponent, children: []},
+      {path: 'posts', component: PostsComponent, children: []},
+      {path: 'comments', component: CommentComponent, children: []}
+
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent,
+    UserDetailsComponent,
+    ComentDetailsComponent,
+    HeaderComponent,
+    MainLayoutComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/coment-details/coment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coment-details',
+  templateUrl: './coment-details.component.html',
+  styleUrls: ['./coment-details.component.css']
+})
+export class ComentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
