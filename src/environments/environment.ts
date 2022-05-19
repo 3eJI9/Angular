1f2d5c1a9a7818c10868243d03f909bbfbfc1ab7
3eJI9 <97410441+3eJI9@users.noreply.github.com>
2022-05-19 11:19:13 -0700

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// @ts-ignore
+// @ts-ignore
 export const environment = {
-  production: false
+  production: false,
+  API: 'https://jsonplaceholder.typicode.com'
 };
 
 /*
